bae3fae64b66115a51141d7e15734b663e1161de
Moiz Ahmed <awanmoiz709@gmail.com>
2026-05-02 12:41:29 +0500

    create react app

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return ( 
+  <div className="bg-background font-heading px-4 py-5  min-h-screen">
+    <div className="w-34 h-34 rounded-2xl bg-primary text-background flex items-center justify-center">Primary Color</div>
+    <div className="w-34 h-34 rounded-2xl bg-secondary mt-2 text-background flex items-center justify-center">secondary Color</div>
+  </div>
+  );
+};
+
+export default App;
